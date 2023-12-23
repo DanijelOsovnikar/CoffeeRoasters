e0d3f9d895a466df5c926b353ff4f0966be56f4d
Danijel Osovnikar <danijel.osovnikar@gmail.com>
2023-12-24 00:05:44 +0100

    HTML for subscribe part done

--- src/app/core/subscribe/subscribe.component.ts
+++ src/app/core/subscribe/subscribe.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.component.html',
-  styleUrls: ['./subscribe.component.scss']
+  styleUrls: ['./subscribe.component.scss'],
 })
-export class SubscribeComponent {
+export class SubscribeComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
 
+  toggleFunc(event: any) {
+    console.log(event);
+    event.target.classList.toggle('active');
+
+    let panel = event.srcElement.nextElementSibling;
+
+    if (panel.style.display === 'flex') {
+      panel.style.display = 'none';
+    } else {
+      panel.style.display = 'flex';
+    }
+  }
 }
